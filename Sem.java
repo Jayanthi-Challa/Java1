0cbbce7c5eab3f918e937b7ead3a2a3a34941b8c
Jayanthi Challa <76255327+Jayanthi-Challa@users.noreply.github.com>
2021-04-25 16:23:03 +0530

    28

---
+++ Sem.java
@@ -0,0 +1,11 @@
+public class Sem{
+	public static void main(String args[]){
+		int A=2, B=2, a, b;
+		a=A+B;
+	    System.out.println(a);
+		b=A*B;
+		System.out.println(b);
+		if(a==b)
+			System.out.print("true");
+	}
+}
